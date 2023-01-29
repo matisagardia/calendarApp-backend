3f869a769eff8efb23fcb33d2dd3821ec17bf218
matisagardia <matisagardia7@gmail.com>
2023-01-29 17:55:37 -0300

    updateEvent controller

--- controllers/events.js
+++ controllers/events.js
@@ -3,7 +3,7 @@ const Event = require('../models/Event');
 
 const getEvent =  async (req, res = response) => {
 
-        const events = await Event.find().populate('user', 'name');
+    const events = await Event.find().populate('user', 'name');
 
     res.json({
         ok: true,
@@ -38,11 +38,58 @@ const createEvent = async (req, res = response) => {
 }
 
 
-const updateEvent = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'updateEvent'
-    })
+const updateEvent = async(req, res = response) => {
+
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        // check if event exists
+
+        const event = await Event.findById(eventId);
+
+        // if it doens't, return an error
+
+        if ( !event ) {
+            res.status(404).json({
+                ok: false,
+                msg: 'Event does not exists with that ID'
+            });
+        }
+
+        // check if the person that created the event matches with the one that is modifying
+
+        if( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'You are not allowed to edit this event'
+            })
+        }
+
+        // if reaches this point, it is OK and the person can edit the event
+
+        const newEvent = {
+            ...req.body,
+            user: uid
+        }
+
+        const updatedEvent = await Event.findByIdAndUpdate(eventId, newEvent, {new: true});
+
+        res.json({
+            ok: true,
+            event: updatedEvent
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact support'
+        });
+    }
+
 }
 
 const deleteEvent = (req, res = response) => {
