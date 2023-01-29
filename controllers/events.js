1acea2e032dc8e5d5a4b09eb1c0215df39177e04
matisagardia <matisagardia7@gmail.com>
2023-01-29 18:25:49 -0300

    deleteEvent controller

--- controllers/events.js
+++ controllers/events.js
@@ -52,7 +52,7 @@ const updateEvent = async(req, res = response) => {
         // if it doens't, return an error
 
         if ( !event ) {
-            res.status(404).json({
+            return res.status(404).json({
                 ok: false,
                 msg: 'Event does not exists with that ID'
             });
@@ -92,12 +92,55 @@ const updateEvent = async(req, res = response) => {
 
 }
 
-const deleteEvent = (req, res = response) => {
-    res.json({
-        ok: true,
-        msg: 'deleteEvent'
-    })
+const deleteEvent = async (req, res = response) => {
+
+    const eventId = req.params.id;
+    const uid = req.uid;
+
+    try {
+
+        // check if event exists
+
+        const event = await Event.findById(eventId);
+
+        // if it doens't, return an error
+
+        if ( !event ) {
+            return res.status(404).json({
+                ok: false,
+                msg: 'Event does not exists with that ID'
+            });
+        }
+
+        // check if the person that created the event matches with the one that is deleting
+
+        if( event.user.toString() !== uid ) {
+            return res.status(401).json({
+                ok: false,
+                msg: 'You are not allowed to delete this event'
+            })
+        }
+
+        // if reaches this point, it is OK and the person can delete the event
+
+        await Event.findByIdAndDelete(eventId);
+
+        res.json({
+            ok: true,
+            msg: 'The event been deleted succesfully'
+        })
+
+        
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            ok: false,
+            msg: 'Contact support'
+        });
+    }
+
 }
+
 
 
 module.exports = {
